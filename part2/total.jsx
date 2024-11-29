0e8985ccfe0615c336dd1ff7a8149f96ea215e74
sjckyla <140221099+sjckyla@users.noreply.github.com>
2024-11-29 13:17:44 +0800

    Delete part2 directory

--- part2/total.jsx
+++
@@ -1,6 +0,0 @@
-const Total = ({ parts }) => {
-    const total = parts.reduce((sum, parts) => sum + parts.exercises, 0)
-    return <p><strong>Total of {total} exercises</strong></p>
-  }
-
-export default Total 
